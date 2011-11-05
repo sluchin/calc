de2f6677428f04bcd7d109e842eb0670dab12277
Tetsuya Higashi <berio.sequenza@gmail.com>
2011-11-05 22:50:32 +0900

    Added test case

---
+++ calc/tests/test_error.c
@@ -0,0 +1,100 @@
+/**
+ * @file  calc/tests/test_error.c
+ * @brief 単体テスト
+ *
+ * @author higashi
+ * @date 2011-11-14 higashi 新規作成
+ * @version \$Id$
+ *
+ * Copyright (C) 2011 Tetsuya Higashi. All Rights Reserved.
+ */
+/* This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <cutter.h> /* cutter library */
+
+#include "def.h"
+#include "util.h"
+#include "log.h"
+#include "error.h"
+
+/* 内部関数 */
+
+/**
+ * get_errormsg() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_errormsg(void)
+{
+
+}
+
+/**
+ * set_errorcode() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_set_errorcode(void)
+{
+
+}
+
+/**
+ * clear_error() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_clear_error(void)
+{
+
+}
+
+/**
+ * is_error() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_is_error(void)
+{
+
+}
+
+/**
+ * check_validate() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_check_validate(void)
+{
+
+}
+
+/**
+ * get_check_math_feexcept() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_check_math_feexcept(void)
+{
+
+}
+
---
+++ calc/tests/test_function.c
@@ -0,0 +1,179 @@
+/**
+ * @file  calc/tests/test_function.c
+ * @brief 単体テスト
+ *
+ * @author higashi
+ * @date 2011-11-14 higashi 新規作成
+ * @version \$Id$
+ *
+ * Copyright (C) 2011 Tetsuya Higashi. All Rights Reserved.
+ */
+/* This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ */
+
+#include <cutter.h> /* cutter library */
+
+#include "def.h"
+#include "util.h"
+#include "log.h"
+#include "function.h"
+
+/* 内部変数 */
+static testfunction func; /**< 関数構造体 */
+
+/* 内部関数 */
+
+/**
+ * 初期化処理
+ *
+ * @return なし
+ */
+void cut_startup(void)
+{
+    test_init_function(&func);
+}
+
+/**
+ * exec_func() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_exec_func(void)
+{
+
+}
+
+/**
+ * get_pow() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_pow(void)
+{
+
+}
+
+/**
+ * get_pi() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_pi(void)
+{
+
+}
+
+/**
+ * get_e() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_e(void)
+{
+
+}
+
+/**
+ * get_rad() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_rad(void)
+{
+
+}
+
+/**
+ * get_deg() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_deg(void)
+{
+
+}
+
+/**
+ * get_sqrt() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_get_sqrt(void)
+{
+
+}
+
+/**
+ * check_math() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_check_math(void)
+{
+
+}
+
+/**
+ * factorial() 関数テスト
+ *
+ * @return なし
+ */
+void
+test_factorial(void)
+{
+
+}
+
+/**
+ * get_factorial() 関数テスト
+ *
+ * @return なし
+ */
+void
+get_factorial(void)
+{
+
+}
+
+/**
+ * get_permutation() 関数テスト
+ *
+ * @return なし
+ */
+void
+get_permutation(void)
+{
+
+}
+
+/**
+ * get_combination() 関数テスト
+ *
+ * @return なし
+ */
+void
+get_combination(void)
+{
+
+}
+
